88df65b6e4970095ee088b1a933f245bffffe67d
DESKTOP-I2AEJKD\bwc <tlstjdtn321@naver.com>
2022-06-13 12:59:21 +0900

    feat: 상수 추가

--- src/main/java/com/example/account/constatnt/Code.java
+++ src/main/java/com/example/account/constatnt/Code.java
@@ -8,7 +8,11 @@ import org.springframework.http.HttpStatus;
 @AllArgsConstructor
 public enum Code {
     SUCESS("0000", "성공하였습니다."),
-    UNREGISTER_SUCCESS("0000", "해지 성공하였습니다.");
+    UNREGISTER_SUCCESS("0000", "해지 성공하였습니다."),
+    DEPOSIT_SUCCESS("0000", "입금 성공하였습니다."),
+    WITHDRAW_SUCCESS("0000", "출금 성공하였습니다."),
+    PAY_SUCCESS("0000", "결제 성공하였습니다."),
+    CANCEL_SUCCESS("0000", "결제 취소에 성공하였습니다.");
 
     private String code;
     private String message;
